98e41f34a8ef7d02c22a962bcdc0561778102c72
[master] <saad.tarik@outlook.com>
2021-05-28 10:49:10 +0100

    App Structure Setup

---
+++ models/fixture.py
@@ -0,0 +1,9 @@
+class fixture:
+
+    def __init__(self, home_team, away_team, fixture_date, fixture_location, league_id, id=None):
+        self.home_team = home_team
+        self.away_team = away_team
+        self.fixture_date = fixture_date
+        self.fixture_location = fixture_location
+        self.league_id = league_id
+        self.id = id
---
+++ models/player.py
@@ -0,0 +1,8 @@
+class Player:
+
+    def __init__(self, player_name, fouls, goals, team_id, id=None):
+        self.player_name = player_name
+        self.fouls = fouls
+        self.goals = goals
+        self.team_id = team_id
+        self.id = id
